d0586e808589ddbef858273ef4e0acbb1d8e25a6
AbdulSobur498 <bellosobur498@gmail.com>
2026-09-05 22:40:18 +0100

    fix import for deployment

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import urlRouters from "./src/routes/index";
+import urlRouters from "./routes/index";
 require("dotenv").config();
 
 const app = express();
